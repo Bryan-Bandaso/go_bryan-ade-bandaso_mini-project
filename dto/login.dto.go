f6a6d49fd8f06e34834ca3c3b3ea03d79185a3d2
Bryan-Bandaso <86877543+Bryan-Bandaso@users.noreply.github.com>
2022-05-23 16:34:21 +0700

    Delete dto directory

--- dto/login.dto.go
+++
@@ -1,6 +0,0 @@
-package dto
-
-type LoginRequest struct {
-	Username string `json:"username" form:"username" binding:"required,username"`
-	Password string `json:"password" form:"password" binding:"required,min=6"`
-}
